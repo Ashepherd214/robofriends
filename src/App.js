b28e91626ad28f0cc9d32bac5b6eb34d0106368c
Aaron S <ashepherd214@gmail.com>
2020-02-12 16:01:50 -0600

    Adding Style
    Background and Title font updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots'
+import './App.css'
 
 
 class App extends Component {
@@ -12,12 +13,20 @@ class App extends Component {
             searchfield: ''
         }
     }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
             </div>
         )
     }
